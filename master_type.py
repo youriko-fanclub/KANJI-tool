6bb7e5d1a59605fd0e0f3de8cad507de66cbd644
direct1889 <s969.6261asthastnkzgfk@gmail.com>
2021-01-01 14:48:22 +0900

    feat: masterdata.tomlから読みだしたfieldの属性情報をちゃんと管理する
    - tomlには `変数名 = 型名:IDかどうか:主キーかどうか` の形式で入稿
        - 型名と各種フラグが1つの文字列になっているので読出時に分離が必要
        - toml入稿時点でそれぞれ別に入れてもいいが、toml記述が冗長になる気がする
    - 従来: python上でもstrのまま保持
    - 変更: 変数情報を管理する専用のクラスMDFieldを追加、フラグをboolで個別に保持

--- master_type.py
+++ master_type.py
@@ -4,10 +4,10 @@
 from pathlib import Path
 import toml
 from repository_path import KanjiPath
+from logging import getLogger, basicConfig, DEBUG
+logger = getLogger(__name__)
 
 class HppGenerator:
-    TYPE_DICT = { 'string': 's3d::String'}
-
     def __init__(self, indent:str):
         self.indent = indent
         self.build_info:str
@@ -17,9 +17,9 @@ class HppGenerator:
         self.class_body:str
 
     def generate(self, data_type_name:str, fields:dict) -> str:
-        ids_hpp_is_required = self.generateClassBody(data_type_name, fields)
+        self.generateClassBody(data_type_name, fields)
         self.generateBuildInfo(data_type_name)
-        self.generatePreProcessor(ids_hpp_is_required)
+        self.generatePreProcessor(fields)
         self.generateNameSpaceBegin()
         self.generateNameSpaceEnd()
         return self.concatenate()
@@ -33,9 +33,13 @@ class HppGenerator:
         return full_text
 
     def generateBuildInfo(self, data_type_name:str):
-        self.build_info = '// This file is generated from %s.toml.\n' % data_type_name
+        self.build_info = '// This file is generated from %s.toml\n' % data_type_name
 
-    def generatePreProcessor(self, need_ids_hpp:bool):
+    def generatePreProcessor(self, fields:dict):
+        need_ids_hpp = False
+        for field in fields.values():
+            if field.is_id:
+                need_ids_hpp = True
         self.preprocessor = '#pragma once\n'
         if need_ids_hpp:
             self.preprocessor += '#include "IDs.hpp"\n'
@@ -48,27 +52,26 @@ class HppGenerator:
         self.namespace_end  = '}\n'
         self.namespace_end += '}\n'
 
-    # class の中身をさらうついでにIDs.hppのincludeが必要を調べて返す
-    def generateClassBody(self, data_type_name:str, fields:dict) -> bool:
-        ids_hpp_is_required = False
-        getter = ''
-        field = ''
+    def generateClassBody(self, data_type_name:str, field_dict:dict):
+        getters = ''
+        fields = ''
         ctor_declaration = self.indent + 'Master%s(' % data_type_name
         ctor_definition = ''
-        for key in fields:
-            if not ids_hpp_is_required and fields[key].startswith('ID:'):
-                ids_hpp_is_required = True
-            field_type, return_type = self.read_type(fields[key])
-            getter += self.indent + '%s %s() const { return m_%s; }\n' % (return_type, self.to_camel_case(key), key)
-            field  += self.indent + '%s m_%s;\n' % (field_type, key)
-            ctor_declaration += '%s %s, ' % (field_type, key)
-            ctor_definition += self.indent + 'm_%s(%s),\n' % (key, key)
+        for field in field_dict.values():
+            field_type, pass_type = field.read_types()
+            # e.g. const s3d::String& label() const { return m_label; }
+            getters += self.indent + '%s %s() const { return m_%s; }\n' % (pass_type, self.to_camel_case(field.name), field.name)
+            # e.g. s3d::String m_label;
+            fields  += self.indent + '%s m_%s;\n' % (field_type, field.name)
+            # e.g. MasterHoge(const s3d::String& label, ... ):
+            ctor_declaration += '%s %s, ' % (pass_type, field.name)
+            # e.g. m_label(label), ... {}
+            ctor_definition += self.indent + 'm_%s(%s),\n' % (field.name, field.name)
         ctor_declaration = ctor_declaration.rstrip(', ')
         ctor_declaration += ') :\n'
         ctor_definition = ctor_definition.rstrip(',\n')
         ctor_definition += ' {}\n'
-        self.generateClassBodyImpl(data_type_name, getter, field, ctor_declaration + ctor_definition)
-        return ids_hpp_is_required
+        self.generateClassBodyImpl(data_type_name, getters, fields, ctor_declaration + ctor_definition)
 
     def generateClassBodyImpl(self, data_type_name:str, getter:str, field:str, constructor:str):
         self.class_body  = 'class Master%s {\n' % data_type_name
@@ -80,37 +83,74 @@ class HppGenerator:
         self.class_body += constructor
         self.class_body += '};\n'
 
-    def read_type(self, type_string:str):
-        prefix_id = 'ID:'
-        heavy_object = [ 'string' ]
-        if type_string.startswith(prefix_id):
-            field_type = type_string.lstrip(prefix_id) + 'ID'
-        else:
-            if type_string in HppGenerator.TYPE_DICT:
-                field_type = HppGenerator.TYPE_DICT[type_string]
-            else:
-                field_type = type_string
-        if type_string in heavy_object:
-            return_type = 'const %s&' % field_type
-        else:
-            return_type = field_type
-        return (field_type, return_type)
-
     def to_camel_case(self, snake_str:str):
         first, *others = snake_str.split('_')
         return ''.join([first.lower(), *map(str.title, others)])
 
-class MasterDataTypeInfo:
-    INDENT = '    '
+class MDField:
+    TYPE_DICT = { 'string': 's3d::String'}
+    HEAVY_OBJECT = [ 'string' ]
 
+    def __init__(self, name:str, type_attribute:str):
+        self.name = name
+        type_name, *attributes = type_attribute.split(':')
+        self.type_name = type_name
+        self.is_id = False
+        self.is_primary_key = False
+        for attribute in attributes:
+            if attribute == 'ID':
+                self.is_id = True
+            if attribute == 'PKey':
+                self.is_primary_key = True
+
+    def read_types(self) -> (str, str):
+        # メンバ変数の型
+        if self.is_id:
+            field_type = self.type_name + 'ID'
+        else:
+            if self.type_name in MDField.TYPE_DICT:
+                field_type = MDField.TYPE_DICT[self.type_name]
+            else:
+                field_type = self.type_name
+        # 引数/返り値の型
+        if self.type_name in MDField.HEAVY_OBJECT:
+            pass_type = 'const %s&' % field_type
+        else:
+            pass_type = field_type
+        return (field_type, pass_type)
+
+
+class MasterDataTypeInfo:
     def __init__(self, toml:dict):
         self.data_type_name = toml['data_type_name']
-        self.fields = toml['field']
+        self.primary_key = None
+        self.fields = self.read_fields(toml['field'])
+
+    def read_fields(self, field_toml:dict):
+        fields = dict()
+        for name, type_attribute in field_toml.items():
+            field = MDField(name, type_attribute)
+            fields[name] = field
+            if field.is_primary_key:
+                if self.primary_key != None:
+                    logger.critical('There are multiple primary keys.')
+                self.primary_key = field.name
+        if self.primary_key == None:
+            logger.critical('There is no primary key.')
+        return fields
 
     def log(self):
         print('// %s --------------------' % self.data_type_name)
-        for key in self.fields:
-            print('%s %s' % (self.fields[key], key))
+        for field in self.fields.values():
+            if field.is_primary_key:
+                primary_icon = '*'
+            else:
+                primary_icon = ' '
+            if field.is_id:
+                constraint = ' (ID: ranged)'
+            else:
+                constraint = ''
+            print('%s%s %s%s' % (primary_icon, field.type_name, field.name, constraint))
         print('// --------------------')
 
     def create_hpp(self, directory:Path):
@@ -120,7 +160,7 @@ class MasterDataTypeInfo:
 
     def output(self, directory:Path, filename:str, text:str):
         file_path = directory / filename
-        print('create file:%s' % file_path.relative_to(KanjiPath.absolute('asset')))
+        logger.info('create file:%s' % file_path.relative_to(KanjiPath.absolute('asset')))
         f = open(file_path,'w') # なければ生成/あれば上書き
         f.write(text)
         f.close()
@@ -152,8 +192,10 @@ class MasterDataTypeManager:
             path_dst = path_dst / key
             for value in self.dict_info[key].values():
                 value.create_hpp(path_dst)
+                value.log()
 
 if __name__ == "__main__":
+    basicConfig(level=DEBUG)
     mgr = MasterDataTypeManager()
     mgr.create_hpp(KanjiPath.absolute('mdheader'))
 
